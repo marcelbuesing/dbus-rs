1bb512de0b33dc56c54c0a8cb72f3a450ed32f40
marcelbuesing <buesing.marcel@googlemail.com>
2018-10-19 14:55:28 +0200

    Further cleanup

--- dbus-tokio/src/adriver.rs
+++ dbus-tokio/src/adriver.rs
@@ -1,15 +1,15 @@
-use std::sync::Mutex;
-use mio::{self, unix, Ready};
+use dbus::{ConnMsgs, Connection, Error as DBusError, Message, MessageType, Watch, WatchEvent};
+use futures::sync::{mpsc, oneshot};
+use futures::{Async, Future, Poll, Stream};
 use mio::unix::UnixReady;
-use std::io;
-use dbus::{Connection, ConnMsgs, Watch, WatchEvent, Message, MessageType, Error as DBusError};
-use futures::{Async, Future, Stream, Poll};
-use futures::sync::{oneshot, mpsc};
-use tokio::reactor::PollEvented2;
-use std::sync::Arc;
-use std::os::raw::c_uint;
+use mio::{self, unix, Ready};
 use std::collections::HashMap;
+use std::io;
+use std::os::raw::c_uint;
 use std::os::unix::io::RawFd;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::reactor::PollEvented2;
 
 type MCallMap = Arc<Mutex<HashMap<u32, oneshot::Sender<Message>>>>;
 
@@ -32,11 +32,13 @@ impl AConnection {
     ///
     /// The task handles incoming messages, and continues to do so until the
     /// AConnection is dropped.
-    pub fn new(c: Arc<Connection>) -> io::Result<AConnection>  {
+    pub fn new(c: Arc<Connection>) -> io::Result<AConnection> {
         let map: MCallMap = Default::default();
         let istream: MStream = Default::default();
 
-        c.set_watch_callback(Box::new(|_| unimplemented!("Watch handling is very rare and not implemented yet")));
+        c.set_watch_callback(Box::new(|_| {
+            unimplemented!("Watch handling is very rare and not implemented yet")
+        }));
 
         let i = AConnection {
             conn: c,
@@ -51,15 +53,11 @@ impl AConnection {
     ///
     /// Creating more than one stream for the same AConnection is not supported; this function will
     /// fail with an error if you try. Drop the first stream if you need to create a second one.
-    pub fn messages(self) -> Result<AMessageStream, &'static str> {
-
-        let (quit_tx, quit_rx) = oneshot::channel();
+    pub fn messages(self) -> Result<ADriver, &'static str> {
         let mut driver = ADriver {
             conn: self.conn.clone(),
             fds: HashMap::new(),
-            quit:  quit_rx,
             callmap: self.callmap.clone(),
-            msgstream: self.msgstream.clone(),
         };
 
         for w in self.conn.watch_fds() {
@@ -69,14 +67,7 @@ impl AConnection {
             })?;
         }
 
-        let (tx, rx) = mpsc::unbounded();
-        {
-            let mut i = driver.msgstream.lock().unwrap();
-            if i.is_some() { return Err("Another instance of AMessageStream already exists"); }
-            *i = Some(tx);
-        }
-
-        Ok(AMessageStream { driver: driver, inner: rx, quit: quit_tx})
+        Ok(driver)
     }
 
     /// Sends a method call message, and returns a Future for the method return.
@@ -87,7 +78,11 @@ impl AConnection {
             let mut map = self.callmap.lock().unwrap();
             map.insert(r, tx); // TODO: error check for duplicate entries. Should not happen, but if it does...
         }
-        let mc = AMethodCall { serial: r, callmap: self.callmap.clone(), inner: rx };
+        let mc = AMethodCall {
+            serial: r,
+            callmap: self.callmap.clone(),
+            inner: rx,
+        };
         Ok(mc)
     }
 }
@@ -104,9 +99,7 @@ impl Drop for AConnection {
 pub struct ADriver {
     conn: Arc<Connection>,
     fds: HashMap<RawFd, PollEvented2<AWatch>>,
-    quit: oneshot::Receiver<()>,
     callmap: MCallMap,
-    msgstream: MStream,
 }
 
 impl ADriver {
@@ -115,62 +108,67 @@ impl ADriver {
         if !w.readable() && !w.writable() {
             self.fds.remove(&w.fd());
         } else {
-
             if let Some(evented) = self.fds.get(&w.fd()) {
                 let ww = evented.get_ref().0;
                 if ww.readable() == w.readable() && ww.writable() == w.writable() {
-                    return Ok(())
+                    return Ok(());
                 };
             }
             self.fds.remove(&w.fd());
 
             let z = PollEvented2::new(AWatch(w));
 
-            if poll_now && z.get_ref().0.readable() { z.clear_read_ready(Ready::readable())?; };
-            if poll_now && z.get_ref().0.writable() { z.clear_write_ready()?; };
+            if poll_now && z.get_ref().0.readable() {
+                z.clear_read_ready(Ready::readable())?;
+            };
+            if poll_now && z.get_ref().0.writable() {
+                z.clear_write_ready()?;
+            };
 
             self.fds.insert(w.fd(), z);
         }
         Ok(())
     }
 
-    fn send_stream(&self, m: Message) {
-        let msgstream = self.msgstream.lock().unwrap();
-        msgstream.as_ref().map(|ref z| { z.unbounded_send(m).unwrap() });
+    /*fn handle_msgs(&mut self) {
+    let msgs = ConnMsgs { conn: self.conn.clone(), timeout_ms: None };
+    for m in msgs {
+        debug!("handle_msgs: {:?}", m);
+        /*if m.msg_type() == MessageType::MethodReturn {
+            let mut map = self.callmap.lock().unwrap();
+            let serial = m.get_reply_serial().unwrap();
+            let r = map.remove(&serial);
+            debug!("Serial {:?}, found: {:?}", serial, r.is_some());
+            if let Some(r) = r { r.send(m).unwrap(); }
+            else { self.send_stream(m) }
+        }*/
     }
-
-    fn handle_msgs(&mut self) {
-        let msgs = ConnMsgs { conn: self.conn.clone(), timeout_ms: None };
-        for m in msgs {
-            debug!("handle_msgs: {:?}", m);
-            if m.msg_type() == MessageType::MethodReturn {
-                let mut map = self.callmap.lock().unwrap();
-                let serial = m.get_reply_serial().unwrap();
-                let r = map.remove(&serial);
-                debug!("Serial {:?}, found: {:?}", serial, r.is_some());
-                if let Some(r) = r { r.send(m).unwrap(); }
-                else { self.send_stream(m) }
-            } else { self.send_stream(m) }
-        }
-    }
+    }*/
 }
 
-impl Future for ADriver {
-    type Item = ();
+impl Stream for ADriver {
+    type Item = Message;
     type Error = ();
 
-    fn poll(&mut self) -> Result<Async<()>, ()> {
-        let q = self.quit.poll();
-        if q != Ok(Async::NotReady) { return Ok(Async::Ready(())); }
-
+    fn poll(&mut self) -> futures::Poll<Option<Self::Item>, Self::Error> {
         for w in self.fds.values() {
             let mut mask = UnixReady::hup() | UnixReady::error();
-            if w.get_ref().0.readable() { mask = mask | Ready::readable().into(); }
+            if w.get_ref().0.readable() {
+                mask = mask | Ready::readable().into();
+            }
             //if w.get_ref().0.writable() { mask = mask | Ready::writable().into(); }
             let prr = w.poll_read_ready(*mask).map_err(|_| ())?;
-            debug!("D-Bus i/o poll read ready: {:?} is {:?}", w.get_ref().0.fd(), prr);
+            debug!(
+                "D-Bus i/o poll read ready: {:?} is {:?}",
+                w.get_ref().0.fd(),
+                prr
+            );
             let pwr = w.poll_write_ready().map_err(|_| ())?;
-            debug!("D-Bus i/o poll write ready: {:?} is {:?}", w.get_ref().0.fd(), pwr);
+            debug!(
+                "D-Bus i/o poll write ready: {:?} is {:?}",
+                w.get_ref().0.fd(),
+                pwr
+            );
             let ur = if let Async::Ready(t) = prr {
                 UnixReady::from(t) | if let Async::Ready(wt) = pwr {
                     UnixReady::from(wt)
@@ -184,17 +182,39 @@ impl Future for ADriver {
                     continue;
                 }
             };
-            let flags =
-                if ur.is_readable() { WatchEvent::Readable as c_uint } else { 0 } +
-                if ur.is_writable() { WatchEvent::Writable as c_uint } else { 0 } +
-                if ur.is_hup() { WatchEvent::Hangup as c_uint } else { 0 } +
-                if ur.is_error() { WatchEvent::Error as c_uint } else { 0 };
+            let flags = if ur.is_readable() {
+                WatchEvent::Readable as c_uint
+            } else {
+                0
+            } + if ur.is_writable() {
+                WatchEvent::Writable as c_uint
+            } else {
+                0
+            } + if ur.is_hup() {
+                WatchEvent::Hangup as c_uint
+            } else {
+                0
+            } + if ur.is_error() {
+                WatchEvent::Error as c_uint
+            } else {
+                0
+            };
+
             debug!("D-Bus i/o unix ready: {:?} is {:?}", w.get_ref().0.fd(), ur);
             self.conn.watch_handle(w.get_ref().0.fd(), flags);
-            if ur.is_readable() { w.clear_read_ready(Ready::readable()).map_err(|_| ())?; };
-            if ur.is_writable() { w.clear_write_ready().map_err(|_| ())?; };
-        };
-        self.handle_msgs();
+            if ur.is_readable() {
+                let mut msgs = ConnMsgs { conn: self.conn.clone(), timeout_ms: None };
+                if let Some(msg) = msgs.next() {
+                    return Ok(Async::Ready(Some(msg)));
+                } else {
+                    w.clear_read_ready(Ready::readable()).map_err(|_| ())?;
+                };
+            };
+
+            if ur.is_writable() {
+                w.clear_write_ready().map_err(|_| ())?;
+            };
+        }
         Ok(Async::NotReady)
     }
 }
@@ -203,27 +223,37 @@ impl Future for ADriver {
 struct AWatch(Watch);
 
 impl mio::Evented for AWatch {
-    fn register(&self,
-                poll: &mio::Poll,
-                token: mio::Token,
-                mut interest: mio::Ready,
-                mut opts: mio::PollOpt) -> io::Result<()>
-    {
-        if !self.0.readable() { interest.remove(mio::Ready::readable()) };
-        if !self.0.writable() { interest.remove(mio::Ready::writable()) };
+    fn register(
+        &self,
+        poll: &mio::Poll,
+        token: mio::Token,
+        mut interest: mio::Ready,
+        mut opts: mio::PollOpt,
+    ) -> io::Result<()> {
+        if !self.0.readable() {
+            interest.remove(mio::Ready::readable())
+        };
+        if !self.0.writable() {
+            interest.remove(mio::Ready::writable())
+        };
         opts.remove(mio::PollOpt::edge());
         opts.insert(mio::PollOpt::level());
         unix::EventedFd(&self.0.fd()).register(poll, token, interest, opts)
     }
 
-    fn reregister(&self,
-                  poll: &mio::Poll,
-                  token: mio::Token,
-                  mut interest: mio::Ready,
-                  mut opts: mio::PollOpt) -> io::Result<()>
-    {
-        if !self.0.readable() { interest.remove(mio::Ready::readable()) };
-        if !self.0.writable() { interest.remove(mio::Ready::writable()) };
+    fn reregister(
+        &self,
+        poll: &mio::Poll,
+        token: mio::Token,
+        mut interest: mio::Ready,
+        mut opts: mio::PollOpt,
+    ) -> io::Result<()> {
+        if !self.0.readable() {
+            interest.remove(mio::Ready::readable())
+        };
+        if !self.0.writable() {
+            interest.remove(mio::Ready::writable())
+        };
         opts.remove(mio::PollOpt::edge());
         opts.insert(mio::PollOpt::level());
         unix::EventedFd(&self.0.fd()).reregister(poll, token, interest, opts)
@@ -247,11 +277,15 @@ impl Future for AMethodCall {
     type Error = DBusError;
 
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
-        let x = self.inner.poll().map_err(|_| DBusError::new_custom("org.freedesktop.DBus.Failed", "Tokio cancelled future"))?;
+        let x = self.inner.poll().map_err(|_| {
+            DBusError::new_custom("org.freedesktop.DBus.Failed", "Tokio cancelled future")
+        })?;
         if let Async::Ready(mut m) = x {
             m.as_result()?;
             Ok(Async::Ready(m))
-        } else { Ok(Async::NotReady) }
+        } else {
+            Ok(Async::NotReady)
+        }
     }
 }
 
@@ -259,40 +293,6 @@ impl Drop for AMethodCall {
     fn drop(&mut self) {
         let mut map = self.callmap.lock().unwrap();
         map.remove(&self.serial);
-    }
-}
-
-#[derive(Debug)]
-/// A Stream of incoming messages.
-///
-/// Messages already processed (method returns for AMethodCall)
-/// are already consumed and will not be present in the stream.
-pub struct AMessageStream {
-    driver: ADriver,
-    inner: mpsc::UnboundedReceiver<Message>,
-    quit: oneshot::Sender<()>,
-}
-
-impl Stream for AMessageStream {
-    type Item = Message;
-    type Error = ();
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        debug!("Polling Driver");
-        self.driver.poll()?;
-        debug!("Polling message stream");
-        let r = self.inner.poll();
-        debug!("msgstream found {:?}", r);
-        r
-    }
-}
-
-impl Drop for AMessageStream {
-    fn drop(&mut self) {
-        {
-            let mut stream = self.driver.msgstream.lock().unwrap();
-            *stream = None;
-        }
-        debug!("Dropping AMessageStream");
     }
 }
 
@@ -304,7 +304,7 @@ fn aconnection_test() {
 
     let mut rt = Runtime::new().unwrap();
 
-   /* let m = ::dbus::Message::new_method_call("org.freedesktop.DBus", "/", "org.freedesktop.DBus", "ListNames").unwrap();
+    /* let m = ::dbus::Message::new_method_call("org.freedesktop.DBus", "/", "org.freedesktop.DBus", "ListNames").unwrap();
     let reply = rt.block_on(aconn.method_call(m).unwrap()).unwrap();
     let z: Vec<&str> = reply.get1().unwrap();
     println!("got reply: {:?}", z);
@@ -331,6 +331,4 @@ fn astream_test() {
     let signals = items.filter_map(|m| if m.msg_type() == ::dbus::MessageType::Signal { Some(m) } else { None });
     let firstsig = rt.block_on(signals.into_future()).map(|(x, _)| x).map_err(|(x, _)| x).unwrap();
     println!("first signal was: {:?}", firstsig);
-    assert_eq!(firstsig.unwrap().msg_type(), ::dbus::MessageType::Signal);*/
-}
-
+    assert_eq!(firstsig.unwrap().msg_type(), ::dbus::MessageType::Signal);*/}
--- dbus-tokio/src/lib.rs
+++ dbus-tokio/src/lib.rs
@@ -12,8 +12,8 @@
 
 extern crate dbus;
 extern crate futures;
-extern crate tokio;
 extern crate mio;
+extern crate tokio;
 
 #[macro_use]
 extern crate log;
@@ -22,4 +22,4 @@ pub mod tree;
 
 mod adriver;
 
-pub use adriver::{AConnection, AMessageStream, AMethodCall};
+pub use adriver::{AConnection, AMethodCall};
